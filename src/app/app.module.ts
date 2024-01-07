c937f5ce3662b5a2a7d2ad9174955772b723c250
Szczepan <szczep.kowalik@gmail.com>
2024-01-07 12:08:45 +0100

    basic project strucutre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TaskComponent } from './task/task.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TaskComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
